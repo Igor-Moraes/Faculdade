21afdf72713b37dff7fbc7ea91b717fd4d53ccd8
Igor Moraes <igorcdemoraes@gmail.com>
2025-10-18 18:04:08 -0300

    formContato

--- Atividades/ex01/website1.js
+++ Atividades/ex01/website1.js
@@ -78,4 +78,14 @@ function divi() {
     resultado.style.fontSize = "20px";
 }
 
-f
+function abrirWhatsapp() {
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let mensagem = document.getElementById('mensagem').value;
+  let numero = "553299702829"; // Substitua pelo seu número com código do país e DDD
+
+  let texto = `Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}`;
+  let link = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+
+  window.open(link);
+}
